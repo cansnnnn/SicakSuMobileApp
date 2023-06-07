29437dbcff105aba080d75204caa665481a395a9
burakmert824 <burakmert824@gmail.com>
2023-06-07 20:17:17 +0300

    add create event button and add to main activity

--- app/src/main/java/com/example/sicaksumobileapp/activities/FeedActivity.java
+++ app/src/main/java/com/example/sicaksumobileapp/activities/FeedActivity.java
@@ -3,8 +3,10 @@ package com.example.sicaksumobileapp.activities;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,8 @@ import com.example.sicaksumobileapp.R;
 import com.example.sicaksumobileapp.SicakSuApp;
 import com.example.sicaksumobileapp.models.SicakSuEvent;
 import com.example.sicaksumobileapp.repository.EventRepo;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
@@ -22,6 +26,7 @@ import java.util.List;
 public class FeedActivity extends AppCompatActivity {
     RecyclerView recView;
     ProgressBar prgBar;
+    FloatingActionButton fab;
 
     Handler handler = new Handler(new Handler.Callback() {
         @Override
@@ -31,7 +36,7 @@ public class FeedActivity extends AppCompatActivity {
             FeedListAdapter adp
                     = new FeedListAdapter(FeedActivity.this,data);
             recView.setAdapter(adp);
-
+            fab.setVisibility(View.VISIBLE);
             prgBar.setVisibility(View.INVISIBLE);
             return true;
         }
@@ -42,7 +47,20 @@ public class FeedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_feed);
         prgBar = findViewById(R.id.feedProgresBar);
         recView = findViewById(R.id.recyclerViewFeed);
+        fab = findViewById(R.id.createEventButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform your desired action here
+                // This code will be executed when the FloatingActionButton is clicked
+                // Add your custom logic or functionality
+
+                // Show a Snackbar with a message
+                Snackbar.make(v, "Create Activity", Snackbar.LENGTH_SHORT).show();
+            }
+        });
         recView.setLayoutManager(new LinearLayoutManager(this));
+        fab.setVisibility(View.INVISIBLE);
         prgBar.setVisibility(View.VISIBLE);
         EventRepo repo = new EventRepo();
         repo.getAllEvents(((SicakSuApp)getApplication()).srv,handler);
