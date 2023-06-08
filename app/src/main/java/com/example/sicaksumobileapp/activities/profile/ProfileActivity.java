830096699146799db628d9e77455665b3b8095a0
burakmert824 <burakmert824@gmail.com>
2023-06-08 19:32:02 +0300

    add logout button

--- app/src/main/java/com/example/sicaksumobileapp/activities/profile/ProfileActivity.java
+++ app/src/main/java/com/example/sicaksumobileapp/activities/profile/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.sicaksumobileapp.activities.profile;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
@@ -19,6 +20,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sicaksumobileapp.R;
 import com.example.sicaksumobileapp.SicakSuApp;
+import com.example.sicaksumobileapp.activities.FeedActivity;
+import com.example.sicaksumobileapp.activities.LoginActivity;
 import com.example.sicaksumobileapp.models.SicakSuProfile;
 import com.example.sicaksumobileapp.models.SicakSuUser;
 import com.example.sicaksumobileapp.repository.EventRepo;
@@ -29,6 +32,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 
 
@@ -36,6 +40,7 @@ public class ProfileActivity extends AppCompatActivity {
 
     private Button btnCreatedEvents;
     private Button btnJoinedEvents;
+    private Button btnLogout;
     private FrameLayout fragmentContainer;
     private String profileId ;
     private ImageView profileImage;
@@ -62,6 +67,7 @@ public class ProfileActivity extends AppCompatActivity {
         btnJoinedEvents = findViewById(R.id.btnJoinedEvents);
         fragmentContainer = findViewById(R.id.fragmentContainer);
         profileImage = findViewById(R.id.profileProfileImage);
+        btnLogout = findViewById(R.id.logoutButton);
 
         // calling the action bar
         ActionBar actionBar = getSupportActionBar();
@@ -70,6 +76,11 @@ public class ProfileActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         String profileId = getIntent().getStringExtra("id");
+        if(Objects.equals(((SicakSuApp) getApplication()).getUserProfile().getId(), profileId)){
+            btnLogout.setVisibility(View.VISIBLE);
+        }else{
+            btnLogout.setVisibility(View.INVISIBLE);
+        }
         // Loginden gelecek
         fetchUserProfile(profileId);
 
@@ -81,7 +92,16 @@ public class ProfileActivity extends AppCompatActivity {
 
 
 
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((SicakSuApp)getApplication()).setUserProfile(new SicakSuProfile());
+                Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
+                startActivity(intent);
+            }
+        });
         // Set click listeners for the buttons
         btnCreatedEvents.setOnClickListener(new View.OnClickListener() {
             @Override
